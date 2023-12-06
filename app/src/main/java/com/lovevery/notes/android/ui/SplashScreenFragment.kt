6ffef4e8d7933eab6a378937a2c0f397adface3d
Jorge E. Hernández <hdez.jeduardo@gmail.com>
2023-12-05 23:36:19 -0600

    ISSUE 4: Setup app navigation, add placeholder TextViews for testing purposes (#19)

--- app/src/main/java/com/lovevery/notes/android/ui/SplashScreenFragment.kt
+++ app/src/main/java/com/lovevery/notes/android/ui/SplashScreenFragment.kt
@@ -1,8 +1,45 @@
 package com.lovevery.notes.android.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.lovevery.notes.android.R
+import com.lovevery.notes.android.databinding.FragmentSplashScreenBinding
 
 class SplashScreenFragment : Fragment() {
+
+    private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.buttonNotes.setOnClickListener {
+            findNavController().navigate(R.id.action_splashScreen_to_usersList)
+        }
+        binding.buttonEmptyNotes.setOnClickListener {
+            findNavController().navigate(R.id.action_splashScreen_to_enterUsername)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     companion object {
         private const val TAG = "SplashScreenFragment"
--- app/src/main/java/com/lovevery/notes/android/ui/users/UsersListFragment.kt
+++ app/src/main/java/com/lovevery/notes/android/ui/users/UsersListFragment.kt
@@ -1,8 +1,24 @@
 package com.lovevery.notes.android.ui.users
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.lovevery.notes.android.databinding.FragmentUsersListBinding
 
 class UsersListFragment : Fragment() {
+
+    private var _binding: FragmentUsersListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUsersListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     companion object {
         private const val TAG = "UsersListFragment"
