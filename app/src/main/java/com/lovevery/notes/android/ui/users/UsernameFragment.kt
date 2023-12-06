3235d0c719c6172d84c3fc27e89b4b5d6fc8c48e
Jorge E. Hernández <hdez.jeduardo@gmail.com>
2023-12-05 23:34:22 -0600

    ISSUE 4: Setup app navigation, add placeholder TextViews for testing purposes

--- app/src/main/java/com/lovevery/notes/android/ui/users/UsernameFragment.kt
+++ app/src/main/java/com/lovevery/notes/android/ui/users/UsernameFragment.kt
@@ -1,8 +1,24 @@
 package com.lovevery.notes.android.ui.users
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.lovevery.notes.android.databinding.FragmentEnterUsernameBinding
 
 class UsernameFragment : Fragment() {
+
+    private var _binding: FragmentEnterUsernameBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEnterUsernameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     companion object {
         private const val TAG = "UsernameFragment"
